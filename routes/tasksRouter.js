b80e680af41e1fa68f5c6099332fcdac4fc2a46a
MaxKorovchenko <max.korovchenko@gmail.com>
2023-05-20 13:16:42 +0300

    bug fix

--- routes/tasksRouter.js
+++ routes/tasksRouter.js
@@ -1,13 +1,26 @@
-const express = require("express");
+const express = require('express');
 
-const { getTask, getTasks, createTask, updateTask, deleteTask } = require("../controllers/tasksControllers");
-const { taskSchema } = require("../utils/validation/taskValidationSchemas");
-const { validateBody } = require("../utils/validateBody");
+const {
+  getTask,
+  getTasks,
+  createTask,
+  updateTask,
+  deleteTask,
+} = require('../controllers/tasksControllers');
+const {
+  taskSchema,
+  updateTaskValidationSchema,
+} = require('../utils/validation/taskValidationSchemas');
+const { validateBody } = require('../utils/validateBody');
 
 const router = express.Router();
 
-router.route("/").get(getTasks).post(createTask);
-router.route("/:taskId").get(getTask).patch(updateTask).delete(deleteTask);
+router.route('/').get(getTasks).post(validateBody(taskSchema), createTask);
+router
+  .route('/:taskId')
+  .get(getTask)
+  .patch(validateBody(updateTaskValidationSchema), updateTask)
+  .delete(deleteTask);
 
 module.exports = {
   tasksRouter: router,
